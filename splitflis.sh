edc3eb1c07e519e4d96e6531c4cb94c272d04a3f
Erich Oelschlegel <erich@ip-172-31-69-124.ec2.internal>
2018-03-16 19:33:16 +0000

    removed 'cut' and replaced with parameter expansion for minor performance increase

--- splitflis.sh
+++ splitflis.sh
@@ -2,19 +2,29 @@
 filename=$1
 splits=$2
 
+prefix="a"
+a="aa"
+b="ab"
+c="ac"
+path="efs/"
+
 echo -e "Analyzing input file $filename"
 lines=$(wc -l < $1)
-#lines=58482519
 echo -e "Total lines: $lines"
+
 i=1
 echo -e "Splits: $splits"
 while [ $i -lt $splits ]
 do
+	#calculate approximate split point
 	mid=$(($lines / $splits * $(($splits - $i)) + 1 ))
 	n=0
+
+	# find next identification record
 	while IFS= read -r line
 	do
-		recordtype=$(cut -c 2 <<< "$line")
+		# read first two bytes
+		recordtype=${line:1:1}
 		if [ $recordtype -eq 1 ]
 		then 
 			echo -e "Breakpoint found at line $(($mid+$n)): $line"
@@ -25,23 +35,26 @@ do
 	done < <(tail -n +"$mid" $filename | head -n 20)
 
 	echo -e "Splitting file ($i of $(($splits-1)))"
-	prefix="a"
 	split -l $(($mid+$n)) $filename $prefix$i
-	path="efs/"
-	a="aa"
-	b="ab"
-	c="ac"
+
+	# move splits over to efs storage so we don't consume disk
 	mv $prefix$i$a $path
-	mv $prefix$i$b $path 
+	mv $prefix$i$b $path
+
+	# point next loop iteration to first half of split 
 	filename=$path$prefix$i$a
+
+	# delete previous input file (unless original) 
 	if [ $i -gt 1 ]
 	then
 		rm $path$prefix$(($i-1))$a
 	fi
 	((i++))
 done
-lastpiece="$prefix$(($i-1))$a"
-mv $path$lastpiece $path$prefix$b
+
+# rename last piece accordingly
+mv $path$prefix$(($i-1))$a $path$prefix$i$b
+
 # for some reason an extra 62 byte "ac" split occurs during the final split.
-cat $prefix$(($i-1))$c >> $path$prefix$b
+cat $prefix$(($i-1))$c >> $path$prefix$i$b
 rm $prefix$(($i-1))$c
